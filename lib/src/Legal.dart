98fc54644519d3d3d2aba52625c4705bda9b0442
Abhinav <abhinavshuklaji1992@gmail.com>
2024-08-09 12:53:34 +0530

    issues fixed again due to view not visible

--- lib/src/Legal.dart
+++ lib/src/Legal.dart
@@ -17,11 +17,8 @@ import 'controller/UpgradeController.dart';
 
 class Legal extends StatelessWidget {
 
-  int selectedOption = 0;
-
-  bool isChecked = false;
-  final controller = Get.put(UpgradeController());
-
+  final int selectedOption = 0;
+  final bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -35,178 +32,154 @@ class Legal extends StatelessWidget {
               Get.back();
           },
           ),),
-        body:  Expanded(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 10,),
-                  InkWell(onTap:() {
-                    var arg= [
-                      {"name": 'Legal Notice'},
-                      {"url": 'https://softkwit.in/sex-positions/legal.html'}
-                    ];
-                    Get.to(LegalWebview(),arguments: arg);
-                  },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 40,
-                      margin: EdgeInsets.only(left: 20,right: 20),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: MyColor.gray2,width: 1),
-                           // color: MyColor.golden
-                        ),
-                      child: Center(
-                        child:   Text(
-                          'Legal Notice',
-                          style: TextStyle(color: MyColor.black,
-                              fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 20,),
-                  InkWell(onTap:() {
-                    var arg= [
-                      {"name": 'Privacy Policy'},
-                      {"url": 'https://softkwit.in/sex-positions/privacy.html'}
-                    ];
-                    Get.to(LegalWebview(),arguments: arg);
-                  },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 40,
-                      margin: EdgeInsets.only(left: 20,right: 20),
-                      decoration: BoxDecoration(
+        body:  SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 10,),
+              InkWell(onTap:() {
+                var arg= [
+                  {"name": 'Legal Notice'},
+                  {"url": 'https://softkwit.in/sex-positions/legal.html'}
+                ];
+                Get.to(LegalWebview(),arguments: arg);
+              },
+                child: Container(
+                  width: double.maxFinite,
+                  height: 40,
+                  margin: EdgeInsets.only(left: 20,right: 20),
+                    decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: MyColor.gray2,width: 1),
-                        //color: Colors.black
-                      ),
-                      child: Center(
-                        child:   Text(
-                          'Privacy Policy',
-                          style: TextStyle(color: MyColor.black,
-                              fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                        ),
-                      ),
+                       // color: MyColor.golden
+                    ),
+                  child: Center(
+                    child:   Text(
+                      'Legal Notice',
+                      style: TextStyle(color: MyColor.black,
+                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
                     ),
                   ),
-
-                  SizedBox(height: 20,),
-                  InkWell(onTap:() {
-                    var arg= [
-                      {"name": 'Term & Conditions'},
-                      {"url": 'https://softkwit.in/sex-positions/term.html'}
-                    ];
-                    Get.to(LegalWebview(),arguments: arg);
-                  },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 40,
-                      margin: EdgeInsets.only(left: 20,right: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: MyColor.gray2,width: 1),
-                        //color: Colors.black
-                      ),
-                      child: Center(
-                        child:   Text(
-                          'Term & Conditions',
-                          style: TextStyle(color: MyColor.black,
-                              fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 20,),
-                  InkWell(onTap:() {
-                    var arg= [
-                      {"name": 'Disclaimer'},
-                      {"url": 'https://softkwit.in/sex-positions/disclaimer.html'}
-                    ];
-                    Get.to(LegalWebview(),arguments: arg);
-                  },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 40,
-                      margin: EdgeInsets.only(left: 20,right: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: MyColor.gray2,width: 1),
-                        //color: Colors.black
-                      ),
-                      child: Center(
-                        child:   Text(
-                          'Disclaimer',
-                          style: TextStyle(color: MyColor.black,
-                              fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-
-
-                  SizedBox(height: 20,),
-                  InkWell(onTap:() {
-                    var arg= [
-                      {"name": 'Attribution'},
-                      {"url": 'https://softkwit.in/sex-positions/attribution.html'}
-                    ];
-                    Get.to(LegalWebview(),arguments: arg);
-                  },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 40,
-                      margin: EdgeInsets.only(left: 20,right: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: MyColor.gray2,width: 1),
-                        //color: Colors.black
-                      ),
-                      child: Center(
-                        child:   Text(
-                          'Attribution',
-                          style: TextStyle(color: MyColor.black,
-                              fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-
-
-
-
-                ],
+                ),
               ),
-            ),
+
+              SizedBox(height: 20,),
+              InkWell(onTap:() {
+                var arg= [
+                  {"name": 'Privacy Policy'},
+                  {"url": 'https://softkwit.in/sex-positions/privacy.html'}
+                ];
+                Get.to(LegalWebview(),arguments: arg);
+              },
+                child: Container(
+                  width: double.maxFinite,
+                  height: 40,
+                  margin: EdgeInsets.only(left: 20,right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: MyColor.gray2,width: 1),
+                    //color: Colors.black
+                  ),
+                  child: Center(
+                    child:   Text(
+                      'Privacy Policy',
+                      style: TextStyle(color: MyColor.black,
+                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20,),
+              InkWell(onTap:() {
+                var arg= [
+                  {"name": 'Term & Conditions'},
+                  {"url": 'https://softkwit.in/sex-positions/term.html'}
+                ];
+                Get.to(LegalWebview(),arguments: arg);
+              },
+                child: Container(
+                  width: double.maxFinite,
+                  height: 40,
+                  margin: EdgeInsets.only(left: 20,right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: MyColor.gray2,width: 1),
+                    //color: Colors.black
+                  ),
+                  child: Center(
+                    child:   Text(
+                      'Term & Conditions',
+                      style: TextStyle(color: MyColor.black,
+                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20,),
+              InkWell(onTap:() {
+                var arg= [
+                  {"name": 'Disclaimer'},
+                  {"url": 'https://softkwit.in/sex-positions/disclaimer.html'}
+                ];
+                Get.to(LegalWebview(),arguments: arg);
+              },
+                child: Container(
+                  width: double.maxFinite,
+                  height: 40,
+                  margin: EdgeInsets.only(left: 20,right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: MyColor.gray2,width: 1),
+                    //color: Colors.black
+                  ),
+                  child: Center(
+                    child:   Text(
+                      'Disclaimer',
+                      style: TextStyle(color: MyColor.black,
+                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+
+
+              SizedBox(height: 20,),
+              InkWell(onTap:() {
+                var arg= [
+                  {"name": 'Attribution'},
+                  {"url": 'https://softkwit.in/sex-positions/attribution.html'}
+                ];
+                Get.to(LegalWebview(),arguments: arg);
+              },
+                child: Container(
+                  width: double.maxFinite,
+                  height: 40,
+                  margin: EdgeInsets.only(left: 20,right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: MyColor.gray2,width: 1),
+                    //color: Colors.black
+                  ),
+                  child: Center(
+                    child:   Text(
+                      'Attribution',
+                      style: TextStyle(color: MyColor.black,
+                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+
+
+
+
+            ],
           ),
         ),
       ),
     );
-  }
-
-  void callAPI(BuildContext context) async
-  {
-    var param = {
-      "username": sp.getEmail(),
-    };
-    //controller.login(param, context);
-    final user = await controller.openLevel(param, context);
-    if(user.status==1)
-    {
-      showToast(user.result!.status!,2);
-      sp.setPaid(1);
-      Get.to(HomeMain());
-    }
-    else
-    {
-      showToast(user.result!.status!,2);
-    }
   }
 
 
--- lib/src/SansualSecretView.dart
+++ lib/src/SansualSecretView.dart
@@ -13,9 +13,9 @@ import 'controller/ScrachViewController.dart';
 
  class SansualSecretView extends  StatelessWidget {
 
-          var data = Get.arguments;
-          final controller = Get.put(SansualController());
-        bool skrach = false;
+      final  data = Get.arguments;
+       final controller = Get.put(SansualController());
+
 
           @override
           Widget build(BuildContext context) {
@@ -31,92 +31,90 @@ import 'controller/ScrachViewController.dart';
                   builder: (_) => controller.isLoading? SizedBox(
                     height: MediaQuery.of(context).size.height / 1.3,
                     child: Center(child: CircularProgressIndicator(),),) :
-                  Expanded(
-                    child: Column(
-                      children: [
-                        SizedBox(height: 30,),
-                        Center(
-                          child:Text(
-                            'Scratch the question with your finger',
-                            style: TextStyle(color: MyColor.black,
-                                fontSize: 18,letterSpacing: 1,fontWeight: FontWeight.bold),
-                          ),
+                  Column(
+                    children: [
+                      SizedBox(height: 30,),
+                      Center(
+                        child:Text(
+                          'Scratch the question with your finger',
+                          style: TextStyle(color: MyColor.black,
+                              fontSize: 18,letterSpacing: 1,fontWeight: FontWeight.bold),
                         ),
-                        SizedBox(height: 30,),
-                        ClipOval(
-                        child: SizedBox.fromSize(
-                        size: Size.fromRadius(MediaQuery.of(context).size.width*0.38),
-                          child: Scratcher(
-                            brushSize: 50,
-                            threshold: 50,
-                            color: Colors.red,
-                            image: Image.asset(
-                              "assets/image/scrach1.jpg",
-                              fit: BoxFit.fill,
-                            ),
-                            onChange: (value) => print("Scratch progress: $value%"),
-                            onThreshold: () => controller.setVisible(true),
-                            child: Container(
-                              height: 300,
-                              width: 300,
-                              color: Colors.white,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Padding(
-                                        padding:EdgeInsets.all(10),
-                                        child: Text(
-                                          controller.modelGetLevel!.result!.tag!,
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 20,
-                                          ),
+                      ),
+                      SizedBox(height: 30,),
+                      ClipOval(
+                      child: SizedBox.fromSize(
+                      size: Size.fromRadius(MediaQuery.of(context).size.width*0.38),
+                        child: Scratcher(
+                          brushSize: 50,
+                          threshold: 50,
+                          color: Colors.red,
+                          image: Image.asset(
+                            "assets/image/scrach1.jpg",
+                            fit: BoxFit.fill,
+                          ),
+                          onChange: (value) => print("Scratch progress: $value%"),
+                          onThreshold: () => controller.setVisible(true),
+                          child: Container(
+                            height: 300,
+                            width: 300,
+                            color: Colors.white,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    Padding(
+                                      padding:EdgeInsets.all(10),
+                                      child: Text(
+                                        controller.modelGetLevel!.result!.tag!,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 20,
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                        )),
-                        SizedBox(height: 10,),
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Visibility(
-                              visible: controller.isVisible,
-                              child: InkWell(
-                                onTap: (){
-                                  callAPI(context);
-                                },
-                                child: Container(
-                                  width: double.maxFinite,
-                                  height: 50,
-                                  margin: EdgeInsets.only(left: 20,right: 20),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      border: Border.all(color: MyColor.gray2,width: 1),
-                                      color: MyColor.black
-                                  ),
-                                  child: Center(
-                                    child:   Text(
-                                      'Next',
-                                      style: TextStyle(color: MyColor.golden,
-                                          fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
-                                    ),
+                        ),
+                      )),
+                      SizedBox(height: 10,),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Visibility(
+                            visible: controller.isVisible,
+                            child: InkWell(
+                              onTap: (){
+                                callAPI(context);
+                              },
+                              child: Container(
+                                width: double.maxFinite,
+                                height: 50,
+                                margin: EdgeInsets.only(left: 20,right: 20),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: MyColor.gray2,width: 1),
+                                    color: MyColor.black
+                                ),
+                                child: Center(
+                                  child:   Text(
+                                    'Next',
+                                    style: TextStyle(color: MyColor.golden,
+                                        fontSize: 16,letterSpacing: 1,fontWeight: FontWeight.bold),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        SizedBox(height: 40,),
-                      ],
-                    ),
+                      ),
+                      SizedBox(height: 40,),
+                    ],
                   ),)
 
 
